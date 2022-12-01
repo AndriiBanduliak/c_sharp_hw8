8918ed10e18fcd4846df8ca8e5e967af8d8f86f9
Andrii Banduliak <banduliak1303@gmail.com>
2022-12-01 21:51:51 +0300

    hw # 8 done

---
+++ 1.cs
@@ -0,0 +1,66 @@
+﻿using static System.Console;
+Clear();
+
+
+int m = InputNumbers("Enter the number of rows in the array: ");
+int n = InputNumbers("Enter the number of columns in the array: ");
+int range = InputNumbers("Enter range: from 0 to ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+WriteLine();
+
+WriteLine($"Sorted array: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Write(input);
+    int output = Convert.ToInt32(ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        WriteLine();
+    }
+}
